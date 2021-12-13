d7c2627eb25be9b0d21831a5044556c55644bed3
Amutha varsni R <amuthavarsni.cs18@bitsathy.ac.in>
2021-12-13 15:32:56 +0530

    Terraform docs

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "tag_owner" {
+  description = "E-mail id of the owner"
   type = string
 }
 
 variable "tag_bucket_environment" {
   type = string
+  description = "Environment details"
 }
 
 variable "tag_bucket_name" {
   type = string
+  description = "Unique name for the bucket"
 }
